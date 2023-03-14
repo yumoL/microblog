c0b30643464ed0a15bae8798fdb00d2d6686ca48
Yumo Luo <yumo.luo@helsinki.fi>
2023-03-14 09:38:02 +0200

    feat:demo the use of koa routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,14 +6,32 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+// router.get('/string', async (ctx, next) => {
+//   ctx.body = 'koa2 string'
+// })
 
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
   }
 })
+
+router.get('/profile/:userName', async(ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async(ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is loadMore API',
+    userName,
+    pageIndex
+  }
+})
+
 
 module.exports = router

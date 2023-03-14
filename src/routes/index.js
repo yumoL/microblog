962574140ced4e028737f566091d04f516e4cf70
Yumo Luo <yumo.luo@helsinki.fi>
2023-03-14 10:08:20 +0200

    feat: demo the use of ejs

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,22 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: false,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaa'
+      },
+      {
+        id: 2,
+        title: 'bbb'
+      },
+      {
+        id: 3,
+        title: 'ccc'
+      }
+    ]
   })
 })
 

887fd9a6b52af7384c743ac1422a29d740c339b8
Yumo Luo <yumo.luo@helsinki.fi>
2023-03-17 23:25:08 +0200

    feat:added session configuration

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./config/db')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -22,6 +26,22 @@ app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
+}))
+
+// session config
+app.keys = ['loPX_?#']
+app.use(session({
+  key: 'weibo.sid', //cookie key in the http header
+  prefix: 'weibo:sess:', //prefix of the redis key, e.g., weibo:sess:ZkrNvEYY9bmUg0LHj00pyN_1XZsxN6LX
+  cookie: {
+    path: '/', //cookie can be used in all APIs of the server
+    httpOnly: true, //cookie can only be changed in the server
+    maxAge: 24 * 60 * 60 * 1000 //expiration time (ms)
+  },
+  //ttl of redis key is defaulted to maxAge
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
 }))
 
 // logger

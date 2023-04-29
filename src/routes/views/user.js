79fd6619822a1ecc8be91741ac1bbd4fc83f65f3
Yumo Luo <yumo.luo@helsinki.fi>
2023-04-29 10:28:56 +0300

    fix: redirect to the home page if already logged in

--- src/routes/views/user.js
+++ src/routes/views/user.js
@@ -4,12 +4,30 @@
 
 const router = require('koa-router')()
 
+/**
+ * retrieve whether a user has logged in or not
+ * @param {*} ctx 
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    idLogin: false
+  }
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+  return data
+}
+
 router.get('/login', async(ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async(ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router

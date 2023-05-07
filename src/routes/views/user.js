d547c8c0899e93dba610cd6d2807609b6ffd5452
Yumo Luo <yumo.luo@helsinki.fi>
2023-05-07 18:02:26 +0300

    feat: user setting UI

--- src/routes/views/user.js
+++ src/routes/views/user.js
@@ -3,6 +3,7 @@
  */
 
 const router = require('koa-router')()
+const { loginRedirect } = require('../../middlewares/loginChecks')
 
 /**
  * retrieve whether a user has logged in or not
@@ -28,6 +29,10 @@ router.get('/login', async(ctx, next) => {
 
 router.get('/register', async(ctx, next) => {
   await ctx.render('register', getLoginInfo(ctx))
+})
+
+router.get('/setting', loginRedirect, async(ctx, next) => {
+  await ctx.render('setting', ctx.session.userInfo)
 })
 
 module.exports = router

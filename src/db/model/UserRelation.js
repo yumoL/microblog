d02dc5c0df7c6a498ae279b4b0a75436f499ba98
Yumo Luo <yumo.luo@helsinki.fi>
2023-06-18 16:59:12 +0300

    feat: create db model for following relation

---
+++ src/db/model/UserRelation.js
@@ -0,0 +1,16 @@
+const seq = require('../seq')
+const { INTEGER } = require('../types')
+
+const UserRelation = seq.define('userRelation', {
+  userId: {
+    type: INTEGER,
+    allowNull: false
+  },
+  followerId: {
+    type: INTEGER,
+    allowNull: false,
+    comment: "the person followed by the user"
+  }
+})
+
+module.exports = UserRelation
